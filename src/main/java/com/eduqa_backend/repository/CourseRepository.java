345a6378a94526f50afa28a7a1be86cde2ae4d54
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-09 16:50:04 +0100

    add find by name jpa

--- src/main/java/com/eduqa_backend/repository/CourseRepository.java
+++ src/main/java/com/eduqa_backend/repository/CourseRepository.java
@@ -2,10 +2,14 @@ package com.eduqa_backend.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eduqa_backend.modal.Course;
 
 public interface CourseRepository extends JpaRepository<Course,UUID>{
+
+    Page<Course> findAllByName(PageRequest of);
 
 }
