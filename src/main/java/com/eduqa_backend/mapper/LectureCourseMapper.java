cfb89e2c0ccb17b59d4f265540faff3d497d1652
michel.igiraneza@auca.ac.rw <amal.healthafrica@gmail.com>
2024-09-09 19:56:42 +0300

    add lecture course mapper

---
+++ src/main/java/com/eduqa_backend/mapper/LectureCourseMapper.java
@@ -0,0 +1,12 @@
+package com.eduqa_backend.mapper;
+import java.util.function.Function;
+
+import com.eduqa_backend.dto.LectureCourseDTO;
+import com.eduqa_backend.modal.LectureCourse;
+public class LectureCourseMapper implements Function<LectureCourse,LectureCourseDTO>{
+    @Override
+    public LectureCourseDTO apply(LectureCourse t) {
+        return new LectureCourseDTO(t.getUser(), t.getCourse());
+    }
+
+}
