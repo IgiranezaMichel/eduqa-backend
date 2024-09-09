b19d3e5eb6328e439ffb011d2eda156a3fc66d0e
michel.igiraneza@auca.ac.rw <amal.healthafrica@gmail.com>
2024-09-09 20:38:52 +0300

    add semester course controller

---
+++ src/main/java/com/eduqa_backend/controller/SemesterCourseController.java
@@ -0,0 +1,27 @@
+package com.eduqa_backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.eduqa_backend.dto.Pagination;
+import com.eduqa_backend.dto.SemesterCourseDTO;
+import com.eduqa_backend.input.SemesterCourseInput;
+import com.eduqa_backend.services.SemesterCourseServices;
+import com.eduqa_backend.util.PageInput;
+
+@RestController
+@RequestMapping("/api/semester-courses")
+public class SemesterCourseController {
+@Autowired SemesterCourseServices semesterCourseService;
+@PostMapping("register")
+public ResponseEntity<String> registerSemesterCourse(@RequestBody SemesterCourseInput registrationInput) {
+return semesterCourseService.registerSemesterCourse(registrationInput);
+}
+@PostMapping("get/all/registration-history")
+public Pagination<SemesterCourseDTO> getAllSemesterRegisteredPage(@RequestBody PageInput input) {
+    return semesterCourseService.getAllSemesterRegisteredPage(input);
+}
+}
