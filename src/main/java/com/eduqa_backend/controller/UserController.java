da4b659223c01320d8998913b9a10458c00d0535
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-09 16:49:26 +0100

    remove spacing

--- src/main/java/com/eduqa_backend/controller/UserController.java
+++ src/main/java/com/eduqa_backend/controller/UserController.java
@@ -2,20 +2,15 @@ package com.eduqa_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
 import com.eduqa_backend.dto.Pagination;
 import com.eduqa_backend.dto.UserDTO;
 import com.eduqa_backend.input.UserInput;
 import com.eduqa_backend.services.UserServices;
 import com.eduqa_backend.util.PageInput;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
