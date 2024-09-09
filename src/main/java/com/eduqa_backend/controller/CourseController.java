f9ac5b8612c40da688c2c88621adc7023cc60b2a
michel.igiraneza@auca.ac.rw <amal.healthafrica@gmail.com>
2024-09-09 19:55:36 +0300

    add routes

--- src/main/java/com/eduqa_backend/controller/CourseController.java
+++ src/main/java/com/eduqa_backend/controller/CourseController.java
@@ -5,10 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eduqa_backend.dto.CourseDTO;
+import com.eduqa_backend.dto.Pagination;
 import com.eduqa_backend.modal.Course;
 import com.eduqa_backend.services.CourseServices;
 import com.eduqa_backend.util.PageInput;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/course")
 public class CourseController {
 @Autowired private CourseServices courseServices;
-public @PostMapping("register")
+@PostMapping("register")
 public ResponseEntity<String> registerCourse(@RequestBody Course entity) {    
     return courseServices.courseRegistreation(entity);
 }
-public @GetMapping("get/all")
-public ResponseEntity<String> getAllCoursespage(@RequestBody PageInput pageInput) {    
+@GetMapping("get/all")
+public Pagination<CourseDTO> getAllCoursespage(@RequestBody PageInput pageInput) {    
     return courseServices.getCoursePage(pageInput);
 }
 }
