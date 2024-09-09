7c16a641a1bbb93b67fb7ddf4bfd11d30c08401f
michel.igiraneza@auca.ac.rw <amal.healthafrica@gmail.com>
2024-09-09 20:49:02 +0300

    add semester repository

--- src/main/java/com/eduqa_backend/repository/SemesterRepository.java
+++ src/main/java/com/eduqa_backend/repository/SemesterRepository.java
@@ -1,10 +1,15 @@
 package com.eduqa_backend.repository;
 
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.eduqa_backend.modal.Semester;
 @Repository
 public interface SemesterRepository extends JpaRepository<Semester,UUID>{
+
+    Page<Semester> findAllByName(PageRequest of);
 
 }
