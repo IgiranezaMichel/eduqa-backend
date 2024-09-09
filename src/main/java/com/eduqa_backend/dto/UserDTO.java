7b48835bcb7a40e51576c7ec405f3450caf2d8f9
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-09 16:03:39 +0100

    add user DTO

---
+++ src/main/java/com/eduqa_backend/dto/UserDTO.java
@@ -0,0 +1,34 @@
+package com.eduqa_backend.dto;
+
+import java.time.format.DateTimeFormatter;
+import com.eduqa_backend.enums.Role;
+import com.eduqa_backend.modal.User;
+import com.eduqa_backend.util.ImageConverter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+private String id;
+private String name;
+private String gender;
+private String phoneNumber;
+private String email;
+private String picture;
+private Role role;
+private String timeStamp;
+public UserDTO(User user) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a");
+    this.id = user.getId().toString();
+    this.name = user.getName();
+    this.gender = user.getGender();
+    this.phoneNumber = user.getPhoneNumber();
+    this.email = user.getEmail();
+     this.picture = ImageConverter.convertToBase64(user.getPicture());
+    this.role = user.getRole();
+    this.timeStamp = formatter.format(user.getTimeStamp());
+}
+
+}
