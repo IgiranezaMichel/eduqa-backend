d4b762dc4239ae690d7ee9e3bd0cd91170ee8526
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-09 16:30:59 +0100

    add new jpa

--- src/main/java/com/eduqa_backend/repository/UserRepository.java
+++ src/main/java/com/eduqa_backend/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.eduqa_backend.repository;
 
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.eduqa_backend.modal.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,UUID>{
+
+    Page<User> findAllByName(PageRequest of);
 
 }
