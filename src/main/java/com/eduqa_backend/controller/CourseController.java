1f7e783acc5f977d7f3108b9832a024ffa5ced6c
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-10 16:03:10 +0100

    add course controller

--- src/main/java/com/eduqa_backend/controller/CourseController.java
+++ src/main/java/com/eduqa_backend/controller/CourseController.java
@@ -4,18 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.eduqa_backend.dto.CourseDTO;
 import com.eduqa_backend.dto.Pagination;
 import com.eduqa_backend.modal.Course;
 import com.eduqa_backend.services.CourseServices;
 import com.eduqa_backend.util.PageInput;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
 @RestController
+@CrossOrigin
 @RequestMapping("/api/course")
 public class CourseController {
 @Autowired private CourseServices courseServices;
@@ -23,8 +22,12 @@ public class CourseController {
 public ResponseEntity<String> registerCourse(@RequestBody Course entity) {    
     return courseServices.courseRegistreation(entity);
 }
-@GetMapping("get/all")
+@PostMapping("get/all")
 public Pagination<CourseDTO> getAllCoursespage(@RequestBody PageInput pageInput) {    
     return courseServices.getCoursePage(pageInput);
+}
+@GetMapping("get/tatal")
+public long getTotalCourse() {    
+    return courseServices.countCourse();
 }
 }
