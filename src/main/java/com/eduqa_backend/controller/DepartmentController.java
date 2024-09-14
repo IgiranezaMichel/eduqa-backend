b21901e11cd9f9079c2581a573fee4f2f3d6ba04
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-15 00:07:21 +0300

    add department controller

---
+++ src/main/java/com/eduqa_backend/controller/DepartmentController.java
@@ -0,0 +1,30 @@
+package com.eduqa_backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduqa_backend.dto.DepartmentDTO;
+import com.eduqa_backend.services.DepartmentServices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/department")
+public class DepartmentController {
+    @Autowired private DepartmentServices departmentService;
+    @PostMapping("register")
+    public ResponseEntity<String> registerDepartment(@RequestBody DepartmentDTO entity) {        
+        return departmentService.createDepartment(entity);
+    }
+    @GetMapping("get/all")
+    public List<DepartmentDTO> getAllDepartment() {
+        return departmentService.getAllDepartment();
+    }
+}
