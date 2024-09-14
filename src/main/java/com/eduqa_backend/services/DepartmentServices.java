81d59d5344952d02dedfbd0b45a395a59c62e982
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-15 00:09:40 +0300

    add department services

---
+++ src/main/java/com/eduqa_backend/services/DepartmentServices.java
@@ -0,0 +1,28 @@
+package com.eduqa_backend.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.eduqa_backend.dto.DepartmentDTO;
+import com.eduqa_backend.modal.Department;
+import com.eduqa_backend.repository.DepartmentRepository;
+
+@Service
+public class DepartmentServices {
+@Autowired private DepartmentRepository departmentRepository;
+public ResponseEntity<String> createDepartment(DepartmentDTO entity) {
+   try {
+    departmentRepository.save(new Department(entity));
+    return ResponseEntity.ok("Department created successfully");
+   } catch (Exception e) {
+    return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+   }
+}
+
+public List<DepartmentDTO> getAllDepartment() {
+   return departmentRepository.findAll().stream().map(DepartmentDTO::new).toList();
+}
+}
