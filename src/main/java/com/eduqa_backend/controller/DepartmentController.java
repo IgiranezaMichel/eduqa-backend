072c5cbac5b43633a8250595f88a8f415445122e
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-15 18:04:09 +0200

    add get all user by role route

--- src/main/java/com/eduqa_backend/controller/DepartmentController.java
+++ src/main/java/com/eduqa_backend/controller/DepartmentController.java
@@ -5,17 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduqa_backend.dto.DepartmentDTO;
+import com.eduqa_backend.enums.Role;
 import com.eduqa_backend.services.DepartmentServices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/department")
 public class DepartmentController {
     @Autowired private DepartmentServices departmentService;
@@ -26,5 +30,9 @@ public class DepartmentController {
     @GetMapping("get/all")
     public List<DepartmentDTO> getAllDepartment() {
         return departmentService.getAllDepartment();
+    }
+    @GetMapping("get/all/user")
+    public List<Object[]> findAllUsertWithInDepartment(@RequestParam Role role) {
+        return departmentService.findAllUserWithInDepartment(role);
     }
 }
