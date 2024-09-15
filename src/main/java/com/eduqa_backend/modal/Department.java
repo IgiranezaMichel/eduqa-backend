5e35a12324482ebf96a65189bd2916165a07dfa6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-15 18:04:29 +0200

    add department

--- src/main/java/com/eduqa_backend/modal/Department.java
+++ src/main/java/com/eduqa_backend/modal/Department.java
@@ -3,6 +3,9 @@ package com.eduqa_backend.modal;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.UuidGenerator.Style;
+
 import com.eduqa_backend.dto.DepartmentDTO;
 
 import jakarta.persistence.CascadeType;
@@ -19,13 +22,19 @@ import lombok.NoArgsConstructor;
 @Data
 public class Department {
 @Id
+@UuidGenerator(style = Style.AUTO)
 private UUID id;
 private String name;
 @OneToMany(cascade = CascadeType.REMOVE,fetch = FetchType.LAZY,mappedBy = "department",targetEntity = User.class)
 public List<User>users;
 public Department(DepartmentDTO entity) {
-    this.id=UUID.fromString(entity.getId());
-    if(entity.getName()!=null)throw new RuntimeException("Name is required");
+    if(entity.getId()!=null&&!entity.getId().isEmpty())
+    try {
+        this.id=UUID.fromString(entity.getId());
+    } catch (Exception e) {
+        throw new RuntimeException("Invalid id");
+     }
+    if(entity.getName()==null||entity.getName().isEmpty())throw new RuntimeException("Name is required");
     this.name=entity.getName();
     
     }
