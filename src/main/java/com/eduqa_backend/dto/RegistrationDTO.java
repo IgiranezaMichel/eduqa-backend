24609cabeb4ba4af6e47b09408407c3b97297fff
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-16 16:17:19 +0200

    remove spacing

--- src/main/java/com/eduqa_backend/dto/RegistrationDTO.java
+++ src/main/java/com/eduqa_backend/dto/RegistrationDTO.java
@@ -1,15 +1,11 @@
 package com.eduqa_backend.dto;
-
 import com.eduqa_backend.modal.Semester;
 import com.eduqa_backend.modal.User;
-
 import lombok.Getter;
 import lombok.Setter;
-
 public class RegistrationDTO extends UserDTO{
     @Getter @Setter
 private SemesterDTO semester;
-
 public RegistrationDTO(User user, Semester semester) {
     super(user);
     this.semester = new SemesterDTO(semester);
