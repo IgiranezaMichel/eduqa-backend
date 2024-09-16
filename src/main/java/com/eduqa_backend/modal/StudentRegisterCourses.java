98ccd9b1f37b56d525ac572585621a5faf84bcb7
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-16 16:18:21 +0200

    add StudentRegisterCourses

---
+++ src/main/java/com/eduqa_backend/modal/StudentRegisterCourses.java
@@ -0,0 +1,32 @@
+package com.eduqa_backend.modal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.UuidGenerator.Style;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+@Data
+@NoArgsConstructor
+@Entity
+public class StudentRegisterCourses {
+@Id
+@UuidGenerator(style = Style.AUTO)
+private UUID id;
+@ManyToOne(cascade = CascadeType.REMOVE,fetch = FetchType.LAZY,targetEntity = Registration.class)
+private Registration registration;
+@ManyToOne(cascade = CascadeType.REMOVE,fetch = FetchType.LAZY,targetEntity = Course.class)
+private Course course;
+private LocalDateTime timeStamp;
+public StudentRegisterCourses(String id,Registration registration, Course course) {
+    if(!id.isEmpty()){
+        this.id=UUID.fromString(id);
+    }
+    this.registration = registration;
+    this.course = course;
+    this.timeStamp=LocalDateTime.now();
+}
+}
