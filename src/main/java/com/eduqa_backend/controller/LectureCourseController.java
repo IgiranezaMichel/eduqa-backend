21b5dd45fa09b9bd732c5af653bc8b7ceee10cdb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-16 16:16:54 +0200

    add security to routes

--- src/main/java/com/eduqa_backend/controller/LectureCourseController.java
+++ src/main/java/com/eduqa_backend/controller/LectureCourseController.java
@@ -3,27 +3,38 @@ package com.eduqa_backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eduqa_backend.dto.LectureCourseDTO;
+import com.eduqa_backend.dto.CourseDTO;
 import com.eduqa_backend.dto.Pagination;
 import com.eduqa_backend.input.LectureCourseInput;
 import com.eduqa_backend.services.LectureCourseServices;
 import com.eduqa_backend.util.PageInput;
-
+import java.security.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/api/lectureCourse")
+@RequestMapping("/api/lecture-course")
 public class LectureCourseController {
 @Autowired private LectureCourseServices lectureCourseService;
 @PostMapping("register")
+@PreAuthorize("hasRole('ROLE_ROLE_HOD') or hasRole('ROLE_ADMIN')")
 public ResponseEntity<String> registerLectureCourses(@RequestBody LectureCourseInput data) {    
     return lectureCourseService.registerLectureCourses(data);
 }
-public Pagination<LectureCourseDTO> getLectureCourses(@RequestBody PageInput input) {
-    return lectureCourseService.getLectureCoursesPage(input);
+@PostMapping("all/courses")
+@Secured("ROLE_INSTRUCTOR")
+public Pagination<CourseDTO> getLectureCourses(Principal principal,@RequestBody PageInput input) {
+    return lectureCourseService.getLectureCoursesPage(input,principal);
+}
+@GetMapping("total/courses")
+@Secured("ROLE_INSTRUCTOR")
+public long getTotalLectureCourse(Principal principal) {
+    return lectureCourseService.getTotalLectureCourse(principal);
 }
 }
