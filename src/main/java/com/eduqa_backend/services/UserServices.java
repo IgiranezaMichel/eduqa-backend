b8e96bafd98827239bead8f98a19e919d82daecf
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-19 17:08:31 +0200

    ADD NEW

--- src/main/java/com/eduqa_backend/services/UserServices.java
+++ src/main/java/com/eduqa_backend/services/UserServices.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.eduqa_backend.dto.Pagination;
 import com.eduqa_backend.dto.UserDTO;
 import com.eduqa_backend.enums.Role;
+import com.eduqa_backend.enums.UserStatus;
 import com.eduqa_backend.input.UserInput;
 import com.eduqa_backend.mapper.UserMapper;
 import com.eduqa_backend.modal.Department;
@@ -20,6 +21,7 @@ import com.eduqa_backend.modal.User;
 import com.eduqa_backend.repository.DepartmentRepository;
 import com.eduqa_backend.repository.UserRepository;
 import com.eduqa_backend.util.GeneratePassword;
+import com.eduqa_backend.util.IdGenerator;
 import com.eduqa_backend.util.PageInput;
 
 @Service
@@ -62,6 +64,28 @@ public class UserServices {
           userInput.setPassword(BCrypt.hashpw(userInput.getPassword(),BCrypt.gensalt()));
         }
       }
+      if(userInput.getRole()==Role.ROLE_INSTRACTOR){
+        User user=userRepository.findFirstByRoleOrderByTimeStampDesc(Role.ROLE_INSTRACTOR).orElse(null);
+        if(user==null){
+          userInput.setCode("LEC-00001");
+        }
+        else{
+         var code= user.getCode().split("-")[1];
+          String generatedId=IdGenerator.getNextId(code);
+          userInput.setCode("LEC-"+generatedId);
+        }
+      }
+      if(userInput.getRole()==Role.ROLE_STUDENT){
+        User user=userRepository.findFirstByRoleOrderByTimeStampDesc(Role.ROLE_STUDENT).orElse(null);
+        if(user==null){
+          userInput.setCode("ST-00001");
+        }
+        else{
+         var code= user.getCode().split("-")[1];
+          String generatedId=IdGenerator.getNextId(code);
+          userInput.setCode("ST-"+generatedId);
+        }
+      }
       User user = userRepository.save(new User(userInput, department));
       if (userHasPassword) {
         emailServices.sendUserHavingPasswordEmailConfirmation(user);
@@ -74,20 +98,20 @@ public class UserServices {
     }
   }
 
-  public Pagination<UserDTO> getAllUserPage(PageInput input, Role role) {
+  public Pagination<UserDTO> getAllUserPage(PageInput input, Role role,UserStatus status) {
     if (input.getSearch() != null && !input.getSearch().isEmpty()) {
-      all = userRepository.findAllByRoleAndNameContainingIgnoreCase(
+      all = userRepository.findAllByRoleAndNameContainingIgnoreCaseAndStatus(
           PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())), role,
-          input.getSearch());
+          input.getSearch(),status);
     } else
       all = userRepository
-          .findAllByRole(PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())), role);
+          .findAllByRoleAndStatus(PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())), role,status);
     return new Pagination<>(all.getNumber(), all.getTotalPages(), all.getTotalElements(),
         all.getContent().stream().map(userMapper).toList());
   }
 
-  public long getTotalUserByRole(Role role) {
-    return userRepository.countByRole(role);
+  public long getTotalUserByRole(Role role,UserStatus status) {
+    return userRepository.countByRoleAndStatus(role,status);
   }
   public Optional<User> findByEmail(String email){
     return userRepository.findByEmail(email);
