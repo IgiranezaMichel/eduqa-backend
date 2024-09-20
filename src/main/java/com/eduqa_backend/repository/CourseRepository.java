1cc0822abbbe880d7458eaa79a0a1cb66916c802
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-20 18:04:13 +0200

    findAvailableCourseWithInASemester

--- src/main/java/com/eduqa_backend/repository/CourseRepository.java
+++ src/main/java/com/eduqa_backend/repository/CourseRepository.java
@@ -5,11 +5,13 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.eduqa_backend.modal.Course;
 
 public interface CourseRepository extends JpaRepository<Course,UUID>{
 
     Page<Course> findAllByNameContainingIgnoreCase(PageRequest of,String name);
-
+    @Query("SELECT c  FROM Course c JOIN LectureCourse lc ON lc.course.id = c.id AND lc.semester.id=:semesterId")
+    Page<Course> findAvailableCourseWithInASemester(PageRequest of,UUID semesterId);
 }
