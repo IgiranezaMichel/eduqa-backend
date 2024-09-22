71371b667293c3e5cefc22698ab9051ffd5068f6
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:43:36 +0200

    add getSemesterById

--- src/main/java/com/eduqa_backend/controller/SemesterController.java
+++ src/main/java/com/eduqa_backend/controller/SemesterController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,9 @@ public List<SemesterDTO>getAllSemester(){
 @GetMapping("get/current-semester")
 public  SemesterDTO lastRegisteredSemester(){
     return semesterService.lastRegisteredSemester();
+}
+@GetMapping("get/{id}")
+public SemesterDTO getSemesterById(@PathVariable String id){
+    return semesterService.getSemesterById(id);
 }
 }
