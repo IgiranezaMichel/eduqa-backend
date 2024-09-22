1ab87fe12de37bc34035d337691692a9150eab9a
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:44:07 +0200

    add getDuration

--- src/main/java/com/eduqa_backend/dto/CourseDTO.java
+++ src/main/java/com/eduqa_backend/dto/CourseDTO.java
@@ -12,13 +12,15 @@ public class CourseDTO {
 private String id;
 private String code;
 private String name;
-private byte credit;
+private int credit;
 private String timeStamp;
+private int duration;
 public CourseDTO(Course course) {
     this.id = course.getId().toString();
     this.code = course.getCode();
     this.name = course.getName();
     this.credit = course.getCredit();
+    this.duration = course.getDuration();
     this.timeStamp = DateConverter.LocalDateTimeConverter(course.getTimeStamp(), "dd, MMMM, yyyy MM:ss a");
 }
 
