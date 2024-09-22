888a42e256e34bac7f9e885cf6526d4f434730e0
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:47:01 +0200

    getStudentJoiningLecturePrincipalCourse

--- src/main/java/com/eduqa_backend/services/StudentRegisterCourseServices.java
+++ src/main/java/com/eduqa_backend/services/StudentRegisterCourseServices.java
@@ -1,33 +1,58 @@
 package com.eduqa_backend.services;
 
+import java.security.Principal;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.eduqa_backend.dto.Pagination;
+import com.eduqa_backend.dto.UserDTO;
 import com.eduqa_backend.input.StudentRegisterCourseInput;
-import com.eduqa_backend.modal.Course;
+import com.eduqa_backend.modal.LectureCourse;
 import com.eduqa_backend.modal.Registration;
 import com.eduqa_backend.modal.StudentRegisterCourses;
-import com.eduqa_backend.repository.CourseRepository;
+import com.eduqa_backend.modal.User;
+import com.eduqa_backend.repository.LectureCourseRepository;
 import com.eduqa_backend.repository.RegistrationRepository;
+import com.eduqa_backend.util.PageInput;
 
 @Service
 public class StudentRegisterCourseServices {
-@Autowired private com.eduqa_backend.repository.StudentRegisterCoursesRepository StudentRegisterCoursesRepository;
-@Autowired private RegistrationRepository registrationRepository;
-@Autowired private CourseRepository courseRepository;
-public ResponseEntity<String> createCourseRegistration(StudentRegisterCourseInput entity) {
-try {
-    Registration registration=registrationRepository.findById(UUID.fromString(entity.getRegistrationId())).orElseThrow(()->new RuntimeException("Invalid registration number"));
-    Course course=courseRepository.findById(UUID.fromString(entity.getCourseId())).orElseThrow(()->new RuntimeException("Invalid Course Code"));
-    StudentRegisterCoursesRepository.save(new StudentRegisterCourses(entity.getId(),registration,course));
-    return new ResponseEntity<>("Course saved successfull",HttpStatus.CREATED);
-} catch (Exception e) {
-   return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
-}
+    @Autowired
+    private com.eduqa_backend.repository.StudentRegisterCoursesRepository StudentRegisterCoursesRepository;
+    @Autowired
+    private RegistrationRepository registrationRepository;
+    @Autowired
+    private LectureCourseRepository rCourseRepository;
 
-}
+    public ResponseEntity<String> createCourseRegistration(StudentRegisterCourseInput entity) {
+        try {
+            Registration registration = registrationRepository.findById(UUID.fromString(entity.getRegistrationId()))
+                    .orElseThrow(() -> new RuntimeException("Invalid registration number"));
+            LectureCourse course = rCourseRepository.findById(UUID.fromString(entity.getLectureCourseId()))
+                    .orElseThrow(() -> new RuntimeException("Invalid Course Code"));
+            StudentRegisterCoursesRepository.save(new StudentRegisterCourses(entity.getId(), registration, course));
+            return new ResponseEntity<>("Course saved successfull", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    public Pagination<UserDTO> getStudentJoiningLecturePrincipalCourse(PageInput input, String semesterId,
+            Principal principal) {
+        Page<User> page = StudentRegisterCoursesRepository
+                .findAllStudentJoiningLecturePrincipalCourse(
+                        PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())),
+                        UUID.fromString(semesterId), principal.getName());
+        return new Pagination<>(page.getNumber(), page.getTotalPages(), page.getTotalElements(),
+                page.getContent().stream().map(UserDTO::new).collect(Collectors.toList()));
+    }
 }
