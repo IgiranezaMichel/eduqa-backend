01b96aa5c73ed0ee735bb5456550f1c51a02450a
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:46:24 +0200

    add findByCourseAndGroup

--- src/main/java/com/eduqa_backend/services/LectureCourseServices.java
+++ src/main/java/com/eduqa_backend/services/LectureCourseServices.java
@@ -11,11 +11,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 import com.eduqa_backend.dto.CourseDTO;
-import com.eduqa_backend.dto.LectureCourseDTO;
 import com.eduqa_backend.dto.Pagination;
 import com.eduqa_backend.input.LectureCourseInput;
 import com.eduqa_backend.mapper.CoursesMapper;
-import com.eduqa_backend.mapper.LectureCourseMapper;
+// import com.eduqa_backend.mapper.LectureCourseMapper;
 import com.eduqa_backend.modal.Course;
 import com.eduqa_backend.modal.LectureCourse;
 import com.eduqa_backend.modal.Semester;
@@ -32,7 +31,7 @@ public class LectureCourseServices {
 @Autowired private UserRepository userRepository;
 @Autowired private CourseRepository courseRepository;
 @Autowired private SemesterRepository semesterRepository;
-private LectureCourseMapper lectureCourseMapper = new LectureCourseMapper();
+// private LectureCourseMapper lectureCourseMapper = new LectureCourseMapper();
 private CoursesMapper coursesMapper=new CoursesMapper();
 public ResponseEntity<String> registerLectureCourses(LectureCourseInput data) {
    try {
@@ -40,20 +39,22 @@ public ResponseEntity<String> registerLectureCourses(LectureCourseInput data) {
     User user = userRepository.findById(UUID.fromString(data.getUserId())).orElseThrow(() -> new RuntimeException("Lecture not found"));
     Semester semester = semesterRepository.findById(UUID.fromString(data.getSemesterId())).orElseThrow(() -> new RuntimeException("Semester not found"));
     LectureCourse lectureCourse = lectureCourseRepository.findByUserAndCourseAndGroup(user,course,data.getGroup()).orElse(null);
-    if(lectureCourse != null) {throw new RuntimeException("Lecture Course already registered");}
-    lectureCourseRepository.save(new LectureCourse(user, course,semester));
+    LectureCourse findGroup= lectureCourseRepository.findByCourseAndGroup(course,data.getGroup()).orElse(null);
+    if(lectureCourse != null) {throw new RuntimeException("Lecture has course already!!");}
+    if(findGroup != null) {throw new RuntimeException("course group has taken by"+findGroup.getUser().getName()+"!!");}
+    lectureCourseRepository.save(new LectureCourse(data,user, course,semester));
     return new ResponseEntity<>("Lecture Course registered successfully", HttpStatus.OK);
    } catch (Exception e) {
     return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
    }
    
 }
-public Pagination<CourseDTO> getLectureCoursesPage(PageInput input,Principal principal) {
-    if (input.getSearch()!=null) {
-        Page<LectureCourse>page = lectureCourseRepository.findAllByCourseNameIgnoreCaseAndUserEmail(PageRequest.of(input.getPageNumber(), input.getPageSize(),Sort.by(input.getSortBy())),input.getSearch(),principal.getName());
+public Pagination<CourseDTO> getLectureCoursesPage(PageInput input,String semesterId,Principal principal) {
+    if (input.getSearch()!=null&&!input.getSearch().isEmpty()) {
+        Page<LectureCourse>page = lectureCourseRepository.findAllByCourseNameIgnoreCaseAndUserEmailAndSemesterId(PageRequest.of(input.getPageNumber(), input.getPageSize(),Sort.by(input.getSortBy())),input.getSearch(),principal.getName(),UUID.fromString(semesterId));
         return new Pagination<>(page.getNumber(),page.getTotalPages(),page.getTotalElements(),page.getContent().stream().map(i->new CourseDTO(i.getCourse())).toList());
    }
-   Page<LectureCourse>page = lectureCourseRepository.findAllByUserEmail(PageRequest.of(input.getPageNumber(), input.getPageSize(),Sort.by(input.getSortBy())),principal.getName());
+   Page<LectureCourse>page = lectureCourseRepository.findAllByUserEmailAndSemesterId(PageRequest.of(input.getPageNumber(), input.getPageSize(),Sort.by(input.getSortBy())),principal.getName(),UUID.fromString(semesterId));
    return new Pagination<>(page.getNumber(),page.getTotalPages(),page.getTotalElements(),page.getContent().stream().map(i->new CourseDTO(i.getCourse())).toList());
 
  }
