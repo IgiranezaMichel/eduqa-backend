7407d5bd7f6b3a09dd049984c204295e66ead44b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-22 08:49:44 +0200

    findAllByUserId

--- src/main/java/com/eduqa_backend/repository/LectureCourseRepository.java
+++ src/main/java/com/eduqa_backend/repository/LectureCourseRepository.java
@@ -1,10 +1,12 @@
 package com.eduqa_backend.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.eduqa_backend.modal.Course;
 import com.eduqa_backend.modal.LectureCourse;
@@ -16,5 +18,7 @@ public interface LectureCourseRepository extends JpaRepository<LectureCourse,UUI
     Page<LectureCourse> findAllByUserEmail(PageRequest of, String name);
     long countByUserEmail(String name);
     Optional<LectureCourse> findByUserAndCourseAndGroup(User user, Course course, String group);
+    @Query("SELECT c FROM Course c JOIN LectureCourse lc ON c.id=lc.course.id WHERE lc.user.id=:userId")
+    List<Course> findAllByUserId(UUID userId);
 
 }
