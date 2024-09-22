ec387d84bb4b8910fab8d840e23c79b275928638
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:43:46 +0200

    add getStudentJoiningLecturePrincipalCourse

--- src/main/java/com/eduqa_backend/controller/StudentRegisterCoursesController.java
+++ src/main/java/com/eduqa_backend/controller/StudentRegisterCoursesController.java
@@ -4,20 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eduqa_backend.dto.Pagination;
+import com.eduqa_backend.dto.UserDTO;
 import com.eduqa_backend.input.StudentRegisterCourseInput;
 import com.eduqa_backend.services.StudentRegisterCourseServices;
+import com.eduqa_backend.util.PageInput;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import java.security.*;
 
 @RestController
 @RequestMapping("/api/registered-student")
 public class StudentRegisterCoursesController {
-@Autowired private StudentRegisterCourseServices services;
-@PostMapping("register")
-public ResponseEntity<String> registerStudentInSemestter(@RequestBody StudentRegisterCourseInput entity) {
-    return services.createCourseRegistration(entity);
-}
+    @Autowired
+    private StudentRegisterCourseServices services;
 
+    @PostMapping("register")
+    public ResponseEntity<String> registerStudentInSemestter(@RequestBody StudentRegisterCourseInput entity) {
+        return services.createCourseRegistration(entity);
+    }
+
+    @PostMapping("get/student/{semesterId}")
+    public Pagination<UserDTO> getStudentJoiningLecturePrincipalCourse(@RequestBody PageInput input,@PathVariable String semesterId,
+            Principal principal) {
+        return services.getStudentJoiningLecturePrincipalCourse(input, semesterId, principal);
+    }
 }
