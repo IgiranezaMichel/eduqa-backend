18f77cef3b164213ff36068b44f2247fcab69e4d
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:45:52 +0200

    add findAllStudentJoiningLecturePrincipalCourse

--- src/main/java/com/eduqa_backend/repository/StudentRegisterCoursesRepository.java
+++ src/main/java/com/eduqa_backend/repository/StudentRegisterCoursesRepository.java
@@ -1,10 +1,19 @@
 package com.eduqa_backend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.*;
 import com.eduqa_backend.modal.StudentRegisterCourses;
+import com.eduqa_backend.modal.User;
 @Repository
 public interface StudentRegisterCoursesRepository extends JpaRepository<StudentRegisterCourses,UUID>{
-
+  @Query("""
+            SELECT src.registration.user FROM 
+            StudentRegisterCourses src JOIN LectureCourse lc ON 
+            src.lectureCourse.id = lc.id WHERE lc.semester.id = :semesterId AND lc.user.email = :lectureEmail
+            """)
+    Page<User> findAllStudentJoiningLecturePrincipalCourse(PageRequest of, UUID semesterId,String lectureEmail);
 }
