9102dc523ab70fb03479499ffd6c9eb4dbb998b3
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-09-22 23:47:27 +0200

    add changes

--- src/main/java/com/eduqa_backend/services/UserServices.java
+++ src/main/java/com/eduqa_backend/services/UserServices.java
@@ -33,7 +33,7 @@ public class UserServices {
   private DepartmentRepository departmentRepository;
   @Autowired
   private EmailServices emailServices;
-  // @Autowired private AuthenticationManager authenticationManager;  
+  // @Autowired private AuthenticationManager authenticationManager;
 
   private UserMapper userMapper = new UserMapper();
   private Page<User> all;
@@ -47,13 +47,13 @@ public class UserServices {
   }
 
   public ResponseEntity<String> registerUser(UserInput userInput) {
-    Department department=new Department();
+    Department department = new Department();
     boolean userHasPassword = true;
     String generatedPassword = GeneratePassword.generatePassword();
     try {
-      if((userInput.getRole()!=Role.ROLE_ADMIN)&&userInput.getRole()!=Role.ROLE_HOD)
-          department = departmentRepository.findById(UUID.fromString(userInput.getDepartmentId()))
-          .orElseThrow(() -> new Exception("Department not found"));
+      if ((userInput.getRole() != Role.ROLE_ADMIN) && userInput.getRole() != Role.ROLE_HOD)
+        department = departmentRepository.findById(UUID.fromString(userInput.getDepartmentId()))
+            .orElseThrow(() -> new Exception("Department not found"));
       User user2 = userRepository.findByEmail(userInput.getEmail()).orElse(null);
       if (user2 != null) {
         return new ResponseEntity<>("User already exists", HttpStatus.BAD_REQUEST);
@@ -62,29 +62,27 @@ public class UserServices {
           userInput.setPassword(BCrypt.hashpw(generatedPassword, BCrypt.gensalt()));
           userHasPassword = false;
         } else {
-          userInput.setPassword(BCrypt.hashpw(userInput.getPassword(),BCrypt.gensalt()));
+          userInput.setPassword(BCrypt.hashpw(userInput.getPassword(), BCrypt.gensalt()));
         }
       }
-      if(userInput.getRole()==Role.ROLE_INSTRACTOR){
-        User user=userRepository.findFirstByRoleOrderByTimeStampDesc(Role.ROLE_INSTRACTOR).orElse(null);
-        if(user==null){
+      if (userInput.getRole() == Role.ROLE_INSTRACTOR) {
+        User user = userRepository.findFirstByRoleOrderByTimeStampDesc(Role.ROLE_INSTRACTOR).orElse(null);
+        if (user == null) {
           userInput.setCode("LEC-00001");
-        }
-        else{
-         var code= user.getCode().split("-")[1];
-          String generatedId=IdGenerator.getNextId(code);
-          userInput.setCode("LEC-"+generatedId);
+        } else {
+          var code = user.getCode().split("-")[1];
+          String generatedId = IdGenerator.getNextId(code);
+          userInput.setCode("LEC-" + generatedId);
         }
       }
-      if(userInput.getRole()==Role.ROLE_STUDENT){
-        User user=userRepository.findFirstByRoleOrderByTimeStampDesc(Role.ROLE_STUDENT).orElse(null);
-        if(user==null){
+      if (userInput.getRole() == Role.ROLE_STUDENT) {
+        User user = userRepository.findFirstByRoleOrderByTimeStampDesc(Role.ROLE_STUDENT).orElse(null);
+        if (user == null) {
           userInput.setCode("ST-00001");
-        }
-        else{
-         var code= user.getCode().split("-")[1];
-          String generatedId=IdGenerator.getNextId(code);
-          userInput.setCode("ST-"+generatedId);
+        } else {
+          var code = user.getCode().split("-")[1];
+          String generatedId = IdGenerator.getNextId(code);
+          userInput.setCode("ST-" + generatedId);
         }
       }
       User user = userRepository.save(new User(userInput, department));
@@ -99,35 +97,49 @@ public class UserServices {
     }
   }
 
-  public Pagination<UserDTO> getAllUserPage(PageInput input, Role role,UserStatus status) {
+  public Pagination<UserDTO> getAllUserPage(PageInput input, Role role, UserStatus status) {
     if (input.getSearch() != null && !input.getSearch().isEmpty()) {
       all = userRepository.findAllByRoleAndNameContainingIgnoreCaseAndStatus(
           PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())), role,
-          input.getSearch(),status);
+          input.getSearch(), status);
     } else
       all = userRepository
-          .findAllByRoleAndStatus(PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())), role,status);
+          .findAllByRoleAndStatus(
+              PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSortBy())), role, status);
     return new Pagination<>(all.getNumber(), all.getTotalPages(), all.getTotalElements(),
         all.getContent().stream().map(userMapper).toList());
   }
 
-  public long getTotalUserByRole(Role role,UserStatus status) {
-    return userRepository.countByRoleAndStatus(role,status);
+  public long getTotalUserByRole(Role role, UserStatus status) {
+    return userRepository.countByRoleAndStatus(role, status);
   }
-  public Optional<User> findByEmail(String email){
+
+  public Optional<User> findByEmail(String email) {
     return userRepository.findByEmail(email);
   }
 
-  public List<UserDTO> getAllUserByRoleAndStatus(Role role,UserStatus status) {
-    return userRepository.findAllByRoleAndStatus(role,status).stream().map(userMapper).toList();
+  public List<UserDTO> getAllUserByRoleAndStatus(Role role, UserStatus status) {
+    return userRepository.findAllByRoleAndStatus(role, status).stream().map(userMapper).toList();
   }
-// public User login(LoginInput input) {
-//         authenticationManager.authenticate(
-//                 new UsernamePasswordAuthenticationToken(
-//                         input.getEmail(),
-//                         input.getPassword()));
 
-//         return userRepository.findByEmail(input.getEmail())
-//                 .orElseThrow();
-//     }
+  public ResponseEntity<String> changeUserStatus(String userId, UserStatus status) {
+    try {
+      User user = userRepository.findById(UUID.fromString(userId))
+          .orElseThrow(() -> new RuntimeException("User not found"));
+      user.setStatus(status);
+      userRepository.save(user);
+      return ResponseEntity.ok("User status changed successfully");
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error changing user status");
+    }
+  }
+  // public User login(LoginInput input) {
+  // authenticationManager.authenticate(
+  // new UsernamePasswordAuthenticationToken(
+  // input.getEmail(),
+  // input.getPassword()));
+
+  // return userRepository.findByEmail(input.getEmail())
+  // .orElseThrow();
+  // }
 }
