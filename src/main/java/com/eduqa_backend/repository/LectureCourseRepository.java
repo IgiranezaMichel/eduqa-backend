a9947c58503bf36b8be0f97a673af6e92831ad20
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-09-25 11:54:21 +0200

    remove spacing

--- src/main/java/com/eduqa_backend/repository/LectureCourseRepository.java
+++ src/main/java/com/eduqa_backend/repository/LectureCourseRepository.java
@@ -11,17 +11,26 @@ import org.springframework.stereotype.Repository;
 import com.eduqa_backend.modal.Course;
 import com.eduqa_backend.modal.LectureCourse;
 import com.eduqa_backend.modal.User;
+
 @Repository
-public interface LectureCourseRepository extends JpaRepository<LectureCourse,UUID>{
-    Page<LectureCourse> findAllByCourseNameIgnoreCase(PageRequest of,String name);
+public interface LectureCourseRepository extends JpaRepository<LectureCourse, UUID> {
+    Page<LectureCourse> findAllByCourseNameIgnoreCase(PageRequest of, String name);
+
     Page<LectureCourse> findAllByCourseNameIgnoreCaseAndUserEmail(PageRequest of, String search, String name);
+
     Page<LectureCourse> findAllByUserEmail(PageRequest of, String name);
+
     long countByUserEmail(String name);
+
     Optional<LectureCourse> findByUserAndCourseAndGroup(User user, Course course, String group);
+
     @Query("SELECT c FROM Course c JOIN LectureCourse lc ON c.id=lc.course.id WHERE lc.user.id=:userId")
     List<Course> findAllByUserId(UUID userId);
+
     Optional<LectureCourse> findByCourseAndGroup(Course course, String group);
+
     Page<LectureCourse> findAllByUserEmailAndSemesterId(PageRequest of, String name, UUID fromString);
+
     Page<LectureCourse> findAllByCourseNameIgnoreCaseAndUserEmailAndSemesterId(PageRequest of, String search,
             String name, UUID fromString);
 
