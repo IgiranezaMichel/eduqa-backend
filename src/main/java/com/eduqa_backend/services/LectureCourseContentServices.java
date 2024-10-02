1d892130b3d1175b4bc5d42206a2f8e8029d4a91
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 20:02:38 +0200

    add lecture course content services

---
+++ src/main/java/com/eduqa_backend/services/LectureCourseContentServices.java
@@ -0,0 +1,37 @@
+package com.eduqa_backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.eduqa_backend.dto.LectureCourseContentDTO;
+import com.eduqa_backend.input.LectureCourseContentInput;
+import com.eduqa_backend.modal.LectureCourse;
+import com.eduqa_backend.modal.LectureCourseContent;
+import com.eduqa_backend.repository.LectureCourseContentRepository;
+import com.eduqa_backend.repository.LectureCourseRepository;
+import java.util.*;
+@Service
+public class LectureCourseContentServices {
+@Autowired private LectureCourseContentRepository lectureCourseContentRepository;
+@Autowired private LectureCourseRepository lRepository;
+
+public ResponseEntity<String> create(LectureCourseContentInput entity) {
+   try {
+     LectureCourse lc=lRepository.findById(UUID.fromString(entity.getLectureCourseId())).orElseThrow(()->new RuntimeException("Course not found"));
+     lectureCourseContentRepository.save(new LectureCourseContent(entity, lc));
+return new ResponseEntity<>("Course content saved successful",HttpStatus.CREATED);
+   } catch (Exception e) {
+    return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+   }
+}
+
+public LectureCourseContentDTO getLectureCourseContent(String lectureCourse) {
+   LectureCourse lc=lRepository.findById(UUID.fromString(lectureCourse)).orElse(null);
+	Optional<LectureCourseContent>lecture=lectureCourseContentRepository.findByLectureCourse(lc);
+   return lecture.stream().map(LectureCourseContentDTO::new).findFirst().orElse(null);
+}
+
+
+}
