09da6ce83336c2b9c4126627b912055031f5604b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 20:01:45 +0200

    add lecture course progress report repository

---
+++ src/main/java/com/eduqa_backend/repository/LectureCourseProgressReportRepository.java
@@ -0,0 +1,11 @@
+package com.eduqa_backend.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduqa_backend.modal.LectureCourseProgressReport;
+
+public interface LectureCourseProgressReportRepository extends JpaRepository<LectureCourseProgressReport,UUID> {
+
+}
