547108121ce3d4655618febc2b2c53dd1ee342c8
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 19:57:26 +0200

    sspacing

--- src/main/java/com/eduqa_backend/repository/RegistrationRepository.java
+++ src/main/java/com/eduqa_backend/repository/RegistrationRepository.java
@@ -20,21 +20,26 @@ public interface RegistrationRepository extends JpaRepository<Registration, UUID
 
     @Query("SELECT u  FROM User u JOIN Registration r ON u.id = r.user.id AND r.semester.id=:semesterId where u.role=:role")
     Page<User> getAvailabelUserRegisterdForASemesterPage(PageRequest of, UUID semesterId, Role role);
-    @Query("""
-        SELECT 
-        new com.eduqa_backend.dto.DualValueChartDTO( CASE 
-        WHEN r.user.id IS NOT NULL THEN 'Registered' 
-        ELSE 'Not Registered' 
-        END AS registrationStatus, 
-        COUNT(u.id) AS count)
 
-        FROM User u
-        LEFT JOIN Registration r ON u.id = r.user.id AND r.semester.id =:semesterId
-        where u.role=:role
-        GROUP BY registrationStatus
-            """)
+    @Query("""
+            SELECT
+            new com.eduqa_backend.dto.DualValueChartDTO( CASE
+            WHEN r.user.id IS NOT NULL THEN 'Registered'
+            ELSE 'Not Registered'
+            END AS registrationStatus,
+            COUNT(u.id) AS count)
+
+            FROM User u
+            LEFT JOIN Registration r ON u.id = r.user.id AND r.semester.id =:semesterId
+            where u.role=:role
+            GROUP BY registrationStatus
+                """)
     List<DualValueChartDTO> getSemesterRegistrationCountStatus(Role role, UUID semesterId);
+
     Page<Registration> findAllByUserEmail(String name, PageRequest of);
+
     @Query("SELECT r.semester FROM Registration r WHERE r.user.email=:studentEmail GROUP BY r.semester.name,r.semester")
     List<Semester> findAllStudentRegisteredSemester(String studentEmail);
+
+    Optional<Registration> findByUserEmailAndSemester(String name, Semester semester);
 }
