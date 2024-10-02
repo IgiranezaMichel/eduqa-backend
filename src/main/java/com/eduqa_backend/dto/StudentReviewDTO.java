9beb48f11d81f84ead6361b24843bfdf955ac250
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 19:53:25 +0200

    StudentReviewDTO

---
+++ src/main/java/com/eduqa_backend/dto/StudentReviewDTO.java
@@ -0,0 +1,14 @@
+package com.eduqa_backend.dto;
+import lombok.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentReviewDTO {
+Object id;
+Object courseName;
+Object courseCode;
+Object lectureImage;
+Object lectureName;
+Object lectureEmail;
+Object totalReviews;
+}
