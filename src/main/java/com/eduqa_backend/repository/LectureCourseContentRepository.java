18b71c304676fbc457b7e5978eb3227905f72933
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-02 20:01:21 +0200

    add lecture course content repository

---
+++ src/main/java/com/eduqa_backend/repository/LectureCourseContentRepository.java
@@ -0,0 +1,14 @@
+package com.eduqa_backend.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduqa_backend.modal.LectureCourse;
+import com.eduqa_backend.modal.LectureCourseContent;
+
+public interface LectureCourseContentRepository extends JpaRepository<LectureCourseContent,UUID>{
+    Optional<LectureCourseContent> findByLectureCourse(LectureCourse lc);
+
+}
