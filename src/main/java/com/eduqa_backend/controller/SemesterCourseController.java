e0e1fb2fea4ff6a938d33604098d5f881d812ccb
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-10-04 06:34:48 +0200

    add get courses method

--- src/main/java/com/eduqa_backend/controller/SemesterCourseController.java
+++ src/main/java/com/eduqa_backend/controller/SemesterCourseController.java
@@ -2,6 +2,7 @@ package com.eduqa_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +12,24 @@ import com.eduqa_backend.dto.SemesterCourseDTO;
 import com.eduqa_backend.input.SemesterCourseInput;
 import com.eduqa_backend.services.SemesterCourseServices;
 import com.eduqa_backend.util.PageInput;
-
+import java.util.*;
 @RestController
 @RequestMapping("/api/semester-courses")
 public class SemesterCourseController {
-@Autowired SemesterCourseServices semesterCourseService;
-@PostMapping("register")
-public ResponseEntity<String> registerSemesterCourse(@RequestBody SemesterCourseInput registrationInput) {
-return semesterCourseService.registerSemesterCourse(registrationInput);
+    @Autowired
+    SemesterCourseServices semesterCourseService;
+
+    @PostMapping("register")
+    public ResponseEntity<String> registerSemesterCourse(@RequestBody SemesterCourseInput registrationInput) {
+        return semesterCourseService.registerSemesterCourse(registrationInput);
+    }
+@GetMapping()
+public List<SemesterCourseDTO>getAllSemesterCourse(){
+    return semesterCourseService.getAllSemesterCourses();
 }
-@PostMapping("get/all/registration-history")
-public Pagination<SemesterCourseDTO> getAllSemesterRegisteredPage(@RequestBody PageInput input) {
-    return semesterCourseService.getAllSemesterRegisteredPage(input);
-}
+
+    @PostMapping("get/all/registration-history")
+    public Pagination<SemesterCourseDTO> getAllSemesterRegisteredPage(@RequestBody PageInput input) {
+        return semesterCourseService.getAllSemesterRegisteredPage(input);
+    }
 }
