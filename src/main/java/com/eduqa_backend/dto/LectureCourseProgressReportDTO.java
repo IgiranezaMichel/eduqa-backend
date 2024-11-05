64fd7505a0ec2b6c3e8cd3d5c9101f40728b0f86
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-11-05 05:46:11 +0200

    add new items

--- src/main/java/com/eduqa_backend/dto/LectureCourseProgressReportDTO.java
+++ src/main/java/com/eduqa_backend/dto/LectureCourseProgressReportDTO.java
@@ -1,7 +1,6 @@
 package com.eduqa_backend.dto;
 import com.eduqa_backend.modal.LectureCourseProgressReport;
 import com.eduqa_backend.util.DateConverter;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +12,15 @@ public class LectureCourseProgressReportDTO {
     private String id;
     private double currentChapter;
     private String title;
+    private String courseName;
     private String timeStamp;
+    private int duration;
     public LectureCourseProgressReportDTO(LectureCourseProgressReport in) {
         this.id = in.getId().toString();
         this.currentChapter = in.getCurrentChapter();
         this.title = in.getTitle();
+        this.courseName=in.getLectureCourseContent().getLectureCourse().getCourse().getName();
+        this.duration=in.getLectureCourseContent().getLectureCourse().getCourse().getDuration();
         this.timeStamp = DateConverter.LocalDateTimeConverter(in.getTimeStamp(), "yyy ,MMM-dd a");
     }
 
