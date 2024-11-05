e8266ebf1b90857958c46700e25055e3bd21c75a
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-11-05 05:45:56 +0200

    addd lecture name

--- src/main/java/com/eduqa_backend/dto/LectureCourseProgressCommentDTO.java
+++ src/main/java/com/eduqa_backend/dto/LectureCourseProgressCommentDTO.java
@@ -19,12 +19,16 @@ public class LectureCourseProgressCommentDTO {
     private String comment;
     private String timeStamp;
     private String lectureCourseContentId;
+    private String lectureName;
+    private String courseName;
     public LectureCourseProgressCommentDTO(LectureCourseProgressComment lcpr) {
         this.id = lcpr.getId().toString();
         this.userName =lcpr.getUser().getName();
         this.userEmail=lcpr.getUser().getEmail();
         this.userPhoto =ImageConverter.convertToBase64(lcpr.getUser().getPicture());
         this.comment = lcpr.getMessage();
+        this.courseName=lcpr.getLectureCourseProgressReport().getLectureCourseContent().getLectureCourse().getCourse().getName();
+        this.lectureName=lcpr.getLectureCourseProgressReport().getLectureCourseContent().getLectureCourse().getUser().getName();
         this.lectureCourseContentId=lcpr.getLectureCourseProgressReport().getId().toString();
         this.timeStamp = DateConverter.LocalDateTimeConverter(lcpr.getTimeStamp(), "dd, MMMM-yyyy a");
     }
