1bec4f2b0edfd0f55d0ebdd1e6cd296798e37f7b
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-11-05 05:46:32 +0200

    add findLatestMessageForEachCourseContentReport

--- src/main/java/com/eduqa_backend/repository/LectureCourseProgressCommentRepository.java
+++ src/main/java/com/eduqa_backend/repository/LectureCourseProgressCommentRepository.java
@@ -1,6 +1,7 @@
 package com.eduqa_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.*;
 import com.eduqa_backend.modal.LectureCourseProgressComment;
@@ -8,5 +9,16 @@ import com.eduqa_backend.modal.LectureCourseProgressComment;
 public interface LectureCourseProgressCommentRepository extends JpaRepository<LectureCourseProgressComment,UUID> {
     List<LectureCourseProgressComment> findAllById(UUID fromString);
     List<LectureCourseProgressComment> findAllByLectureCourseProgressReportId(UUID lectureCourseProgressReportId);
+@Query("""
+    SELECT lcpc FROM LectureCourseProgressComment lcpc
+    JOIN lcpc.user u
+    WHERE lcpc.timeStamp = (
+        SELECT MAX(innerLcpc.timeStamp)
+        FROM LectureCourseProgressComment innerLcpc
+        WHERE innerLcpc.lectureCourseProgressReport.lectureCourseContent = lcpc.lectureCourseProgressReport.lectureCourseContent and 
+        lcpc.lectureCourseProgressReport.lectureCourseContent.lectureCourse.semester.id=:semesterId
+    )
+""")
+List<LectureCourseProgressComment> findLatestMessageForEachCourseContentReport(UUID semesterId);
 
 }
